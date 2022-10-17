491ccbbec72bebb903442ce42f2973d93cb72657
Ilie1985 <ilie.marian32@yahoo.com>
2022-10-17 15:58:02 +0100

    css variables

--- src/App.js
+++ src/App.js
@@ -1,4 +1,8 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { FiChevronRight, FiChevronLeft } from "react-icons/fi";
+import { FaQuoteRight } from "react-icons/fa";
+
+import data from "./data";
 
 function App() {
   return (
